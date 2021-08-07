0b43f17387a297da00836385fe524737355a20c1
허신 <ghtls050@gmail.com>
2021-08-07 16:34:03 +0900

    First

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="map"></div>
+
+  <SortSelector id="sort-selector" />
+  
+  <Card class="card" v-for="data in stores" :key="data" :data="data" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import stores from './assets/data.js'
+import Card from './Card.vue'
+import SortSelector from './SortSelector.vue'
 
 export default {
   name: 'App',
+  data() {
+    data : [
+      mounted() {
+        stores = 
+      },
+    ]
+    return {
+      oriStores: stores,
+      stores: [...stores],
+      curKindIdx: 0,
+    }
+  },
+  methods : {
+    setCurKind(idx) {
+      this.curKindIdx = idx;
+      if(this.curKindIdx == 0) {
+        this.stores.sort((a, b) => a.count / a.countMax - b.count / b.countMax);
+      }
+      else if (this.curKindIdx == 1) {
+        this.stores.sort((a, b) => a.distance - b.distance);
+      }
+    },
+    getCurKindMargin() {
+      return { marginLeft : this.curKindIdx * 150 + 'px' }
+    },
+  },
   components: {
-    HelloWorld
+    Card : Card,
+    SortSelector : SortSelector,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+}
+.map {
+  width: 90%; height: 400px;
+  margin: 0 auto;
+  background: lightgray;
+}
+#sort-selector {
+  position: sticky;
+  z-index: 1;
+  top: 10px;
+  margin-top: 10px;
+}
+.card {
+  margin-top: 10px;
 }
 </style>
