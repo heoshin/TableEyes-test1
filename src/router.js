5c6f847e1a5040ca9d70e51cdbc95e2c017f9e4f
허신 <ghtls050@gmail.com>
2021-08-31 11:30:11 +0900

    router에 StoreList 추가

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from './components/Home.vue'
-import Search from './components/Search.vue'
+// import Search from './components/Search.vue'
 import WishList from './components/WishList.vue'
 import StoreList from './components/StoreList.vue'
 import History from './components/History.vue'
 import MyPage from './components/MyPage.vue'
+import StorePage from './components/StorePage.vue'
 
 const routes = [
   {
@@ -17,7 +18,7 @@ const routes = [
   },
   {
     path: "/search",
-    component: Search,
+    component: StorePage,
   },
   {
     path: "/wishList",
